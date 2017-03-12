c2af591660fd44551c8869569e86fb394f864bbd
Kamesh Sampath <kamesh.sampath@hotmail.com>
2017-03-12 23:27:33 +0530

    First Import, working version of Eureka server

---
+++ src/main/java/org/workspace7/springcloud/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package org.workspace7.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author kameshs
+ */
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
